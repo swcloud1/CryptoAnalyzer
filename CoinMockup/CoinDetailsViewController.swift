0ee07a7f9d2f8a8b967e23fe6d7b1acc4e5525c2
swcloud1 <31655146+swcloud1@users.noreply.github.com>
2018-01-12 16:42:29 +0100

    Add files via upload

---
+++ CoinMockup/CoinDetailsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  CoinMockup
+//
+//  Created by Siwa Sardjoemissier on 08/01/2018.
+//  Copyright © 2018 Siwa Sardjoemissier. All rights reserved.
+//
+
+import UIKit
+
+class CoinDetailsViewController: UIViewController {
+
+    @IBAction func watchListPressed(_ sender: Any) {
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
